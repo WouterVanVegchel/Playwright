1c66a346ee364e30e7c6434e844bc298e3e1b415
Wouter van Vegchel <wvanvegchel@hotmail.com>
2022-06-21 14:33:06 +0200

    test

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,6 +8,8 @@ const config: PlaywrightTestConfig = {
     timeout: 500,
   },
   forbidOnly: !!process.env.CI,
+  retries: process.env.CI ? 2 : 0,
+  workers: process.env.CI ? 1 : undefined,
   reporter: "html",
   use: {
     ignoreHTTPSErrors: true,
