9c733778ba80496ac3ed1da14ff1e78abb640c32
Wouter van Vegchel <wvanvegchel@hotmail.com>
2022-06-21 14:11:25 +0200

    small things

---
+++ playwright.config.ts
@@ -0,0 +1,20 @@
+import type { PlaywrightTestConfig } from "@playwright/test";
+import { devices } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+  testDir: "./tests",
+  timeout: 300 * 1000,
+  expect: {
+    timeout: 500,
+  },
+  reporter: "html",
+  projects: [
+    {
+      name: "chromium",
+      use: {
+        ...devices["Desktop Chrome"],
+      },
+    },
+  ],
+  outputDir: "test-results/",
+};
