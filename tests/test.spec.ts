7869eb2094aa523ec26ffd89e9c55b14873d5029
Wouter van Vegchel <wvanvegchel@hotmail.com>
2024-06-13 15:49:59 +0200

    Update test.spec.ts

--- tests/test.spec.ts
+++ tests/test.spec.ts
@@ -1,6 +1,8 @@
 import { expect, test } from "@playwright/test";
 
+const password = "test123";
+
 test("Dummy", async () => {
-  console.log("Start the test =>");
+  console.log("Start the test =>", password);
   await expect(1).toBe(2);
 });
